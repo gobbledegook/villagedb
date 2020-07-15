2871fb8c841d8c3df87ed779bde569049ef386b2
Dominic Yu <gobbledegook@users.noreply.github.com>
2020-07-14 23:46:14 -0700

    update map links to compensate for incorrect Sunwui/Chungshan coordinates

--- util/generate_maploc.pl
+++ util/generate_maploc.pl
@@ -12,17 +12,25 @@ $sth->execute();
 while (my ($id, $heung_rom, $maploc, $old_latlon, $county_id, $county, $area) = $sth->fetchrow_array()) {
 	next unless $maploc;
 	my ($letter1, $easting, $northing) = $maploc =~ /(\w)\w ?(\d\d)(\d\d)/;
+	my $letter2 = 'E'; # E is the new Q!
 	if (defined($northing)) {
 		$letter1 = uc $letter1;
 		if ($county_id == 2) {
 			# for Hoiping, convert from polyconic grid in yards to MGRS in meters
 			($easting, $northing) = ($northing, $easting);
+			$easting = 92 if $easting == 12; # fix Lung Tong
 			$easting = $easting*0.9141 - 8.59;
 			$easting = sprintf "%.0f", $easting; # round to integer
 			$northing +=100 if $northing < 40;
 			$northing = $northing*0.9141 - 23.34;
 			$northing = sprintf "%.0f", $northing;
 			$letter1 = 'F';
+		} elsif ($county_id == 3 || $county_id == 4) {
+			# for Sunwui/Chungshan, adjust vertical coordinate
+			if ($northing < 30) {
+				# they meant GR and not GQ in this case
+				$letter2 = 'F'; # F is the new R!
+			}
 		}
 		$easting++; # just to account for a little bit of of the china gps shift
 		if ($easting > 99) {
@@ -32,7 +40,7 @@ while (my ($id, $heung_rom, $maploc, $old_latlon, $county_id, $county, $area) = 
 		$easting = sprintf("%02i", $easting);
 		$northing = sprintf("%02i", $northing);
 
-		my $mgrs = "49Q${letter1}E$easting$northing";
+		my $mgrs = "49Q$letter1$letter2$easting$northing";
 		my ($lat, $lon) = mgrs_to_latlon(6, $mgrs); # 6 is Clarke 1880
 		my $latlon = sprintf("%.5f,%.5f", $lat, $lon);
 		next if $latlon eq $old_latlon;
